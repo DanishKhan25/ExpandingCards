208a47038c45c1910d9292dfaa55ead4e7283a3d
Danish <danish7766@gmail.com>
2022-10-14 10:14:49 +0530

    Expanding cards

---
+++ script/script.js
@@ -0,0 +1,14 @@
+var panels = document.querySelectorAll(".panel");
+// adding active class in each one by one by removing active class from others
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
